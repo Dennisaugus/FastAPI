a1ab89cfd4fe866f0b2b6bf42d0d5c704ec12a20
Dennisaugus <dennisasg@outlook.com>
2023-04-18 08:32:27 -0300

    Seção 02 - Primeira API

---
+++ secao02/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/msg')
+async def mensagem():
+    return {"msg": "Meu Primeiro APP usando o FastAPI"}
+
+
+@app.get('/')
+async def raiz():
+    return {"msg": "Pagina raiz com alterações"}
+
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, 
+                log_level='info', reload=True)
+
