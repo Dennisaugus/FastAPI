05fe89ad85322ed84a7168f3c41e5ad5c0d7d7cb
Dennisaugus <dennisasg@outlook.com>
2023-04-26 08:55:00 -0300

    Seção3

--- exercicio/main.py
+++ exercicio/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI 
 from fastapi import HTTPException
 from fastapi import status
+from fastapi import Response
+from fastapi import Path
 
 from models import Funcionario
 from typing import List,Optional
@@ -64,7 +66,7 @@ async def get_funcionarios():
     return funcionarios
 
 @app.get('/funcionarios/{funcionario_id}')
-async def get_funcionario(funcionario_id: int):
+async def get_funcionario(funcionario_id: int = Path(title='Id do funcionário: ' ,description='Deve ser entre 1 a 6:', gt=0, lt=7)):
     try:
         funcionario = funcionarios[funcionario_id]
         funcionario.update({"id": funcionario_id})
@@ -79,6 +81,28 @@ async def post_funcionario(funcionario: Funcionario):
     funcionarios[next_id] = funcionario
     del funcionario.id
     return funcionario
+
+@app.put('/funcionarios/{funcionario_id}')
+async def put_funcionario(funcionario_id: int, funcionario: Funcionario):
+    if funcionario_id in funcionarios:
+        funcionarios[funcionario_id] = funcionario
+        del funcionario.id
+
+        return funcionario
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Não existe este funcionario com o id{funcionario_id}')
+
+@app.delete('/funcionarios/{funcionario_id}')
+async def delete_funcionario(funcionario_id: int):
+    if funcionario_id in funcionarios:
+        del funcionarios[funcionario_id]
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Não existe este funcionario com o id{funcionario_id}')
+
+
 
 
 if __name__ == '__main__':
--- secao03/main.py
+++ secao03/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
+from fastapi.responses import JSONResponse
+from fastapi import Response
+from fastapi import Path
 from fastapi import status
+
 from models import Curso
 from typing import List, Optional
 
@@ -33,13 +37,13 @@ async def get_cursos():
 
 # Trás recursos expecíficos 
 @app.get('/cursos/{curso_id}')
-async def get_curso(curso_id: int):
+async def get_curso(curso_id: int = Path(title='ID do curso', description='Deve ser entre 1 e 2', gt=0, lt=3)):
     try:
         curso = cursos[curso_id]
-        curso.update({"id": curso_id})
         return curso 
     except KeyError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado')
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado')
  
 # Novo endpoint
 @app.post('/cursos', status_code=status.HTTP_201_CREATED)
@@ -49,6 +53,38 @@ async def post_curso(curso: Curso):
    del curso.id #Remove a visualização do id null
    return curso
     
+
+@app.put('/cursos/{curso_id}')
+async def put_curso(curso_id: int, curso: Curso):
+    if curso_id in cursos:
+        cursos[curso_id] = curso
+        del curso.id
+
+        return curso
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Não existe este curso com o id {curso_id}')
+    
+
+@app.delete('/cursos/{curso_id}')
+async def delete_curso(curso_id: int):
+    if curso_id in cursos:
+        del cursos[curso_id]
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+       #return JSONResponse(status_code=status.HTTP_204_NO_CONTENT) em faze de bug ainda 0.95.0
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Não existe este curso com o id {curso_id}')
+
+
+@app.get('/calculadora')
+async def calcular(a: int,b: int,c: Optional[int]):
+    soma = a + b
+    if c:
+        soma += c
+        
+    return {"resultado": soma}
+
 
 if __name__ == '__main__':
     import uvicorn
